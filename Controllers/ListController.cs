fef9fe912903074a041c0bbc2bb334330749e7b9
dVlah99 <vlah.danijel37@gmail.com>
2023-09-05 15:10:35 +0200

    gg

--- Controllers/ListController.cs
+++ Controllers/ListController.cs
@@ -40,7 +40,6 @@ namespace test.Controllers
         public async Task<IActionResult> EditItem(string id, string itemName ,[FromBody] Item item)
         {
             await _context.EditItemAsync(id, itemName, item);
-            Console.WriteLine("DELETE THIS!!");
             return NoContent();
         }
 
@@ -48,7 +47,6 @@ namespace test.Controllers
         public async Task<IActionResult> DeleteList(string id)
         {
             await _context.DeleteAsync(id);
-            Console.WriteLine("DELETE THIS!!");
 
             return NoContent();
         }
